5be9ae141bcd605aa778fadd60d2b62791adb1d8
davide <davidemaria.colmi@gmail.com>
2022-02-28 22:46:05 +0100

    List Adapter Implemented

---
+++ app/src/main/java/com/teddyDev/myweather/LocationListAdapter.kt
@@ -0,0 +1,51 @@
+package com.teddyDev.myweather
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.contentValuesOf
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.teddyDev.myweather.database.LocationEntity
+import com.teddyDev.myweather.databinding.*
+import com.teddyDev.myweather.databinding.WeatherMeteoListBinding
+
+class LocationListAdapter: ListAdapter<LocationEntity, LocationListAdapter.LocationViewHolder>(DiffCallback){
+
+    class LocationViewHolder(private var binding: WeatherItemBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(location: LocationEntity){
+            binding.apply {
+                weatherText.text = location.location
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LocationViewHolder {
+        val binding: WeatherItemBinding = WeatherItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return LocationViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: LocationViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object{
+        private val DiffCallback = object: DiffUtil.ItemCallback<LocationEntity>(){
+            override fun areItemsTheSame(
+                oldItem: LocationEntity,
+                newItem: LocationEntity
+            ): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(
+                oldItem: LocationEntity,
+                newItem: LocationEntity
+            ): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+}
--- app/src/main/java/com/teddyDev/myweather/WeatherListFragment.kt
+++ app/src/main/java/com/teddyDev/myweather/WeatherListFragment.kt
@@ -6,11 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.teddyDev.myweather.databinding.WeatherMeteoListBinding
+import com.teddyDev.myweather.viewModel.LocationViewModel
+import com.teddyDev.myweather.viewModel.LocationViewModelFactory
 
 class WeatherListFragment : Fragment() {
     private lateinit var binding: WeatherMeteoListBinding
+
+    private val viewModel: LocationViewModel by activityViewModels {
+        LocationViewModelFactory(
+            (activity?.application as WeatherApplication).appDatabase.getLocationDao()
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,15 +27,29 @@ class WeatherListFragment : Fragment() {
     ): View? {
         binding =  WeatherMeteoListBinding.inflate(layoutInflater, container,false)
 
+
+        binding.apply {
+            addNewLocationButton.setOnClickListener {
+                Toast.makeText(activity, "Button pressed", Toast.LENGTH_SHORT).show()
+                findNavController().navigate(R.id.action_meteoListFragment_to_addLocationFragment)
+            }
+
+            val adapter = LocationListAdapter()
+
+            viewModel.locationList.observe(viewLifecycleOwner){ locations ->
+                locations.let { adapter.submitList(it) }
+            }
+
+            weatherRecycler.adapter = adapter
+
+        }
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.addNewLocationButton.setOnClickListener {
-            Toast.makeText(activity, "Button pressed", Toast.LENGTH_SHORT).show()
-            findNavController().navigate(R.id.action_meteoListFragment_to_addLocationFragment)
-        }
+
     }
 
     companion object {
